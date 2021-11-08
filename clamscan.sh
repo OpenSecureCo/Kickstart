38a711f944c88feb8d87aead8a8b0eed75d6fd90
OpenSecureCo <74164997+OpenSecureCo@users.noreply.github.com>
2021-11-08 09:51:54 -0600

    Create clamscan.sh

---
+++ clamscan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIRTOSCAN="/var/www /home /opt /usr/bin /etc /usr/sbin";
+TODAY=$(date +%u);
+
+if [ "$TODAY" == "6" ];then
+ nice -n5 clamscan -ri / --exclude-dir=/sys/;
+
+else
+ for S in ${DIRTOSCAN}; do
+  DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
+
+  clamscan -ri "$S";
+done
+fi"
