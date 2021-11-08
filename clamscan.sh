2b9aa22b2133185382a78c8b3b4e49c78b9b7d59
OpenSecureCo <74164997+OpenSecureCo@users.noreply.github.com>
2021-11-08 10:37:09 -0600

    Update clamscan.sh

--- clamscan.sh
+++ clamscan.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+clamd
 DIRTOSCAN="/var/www /home /opt /usr/bin /etc /usr/sbin";
 TODAY=$(date +%u);
 
 if [ "$TODAY" == "6" ];then
- nice -n5 clamscan -ri / --exclude-dir=/sys/;
+ nice -n5 clamdscan -iv / --exclude-dir=/sys/;
 
 else
  for S in ${DIRTOSCAN}; do
   DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
 
-  clamscan -ri "$S";
+  clamdscan -iv "$S";
 done
 fi
+ps -ef | grep clamd | grep -v grep | awk '{print $2}' | xargs kill
