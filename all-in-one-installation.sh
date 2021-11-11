ee8a087cb813f20ec59aa200513a37faa08bb27a
OpenSecureCo <74164997+OpenSecureCo@users.noreply.github.com>
2021-11-11 11:56:53 -0600

    Update all-in-one-installation.sh

--- all-in-one-installation.sh
+++ all-in-one-installation.sh
@@ -2,13 +2,13 @@
 service wazuh-agent stop
 yum remove wazuh-agent -y
 rm -rf /var/ossec/
-WAZUH_MANAGER='logs.vervantis.com' WAZUH_AGENT_GROUP='default' yum install https://packages.wazuh.com/4.x/yum/wazuh-agent-4.2.4-1.x86_64.rpm -y
+WAZUH_MANAGER='' WAZUH_AGENT_GROUP='default' yum install https://packages.wazuh.com/4.x/yum/wazuh-agent-4.2.4-1.x86_64.rpm -y
 echo "logcollector.remote_commands=1" >> /var/ossec/etc/local_internal_options.conf
 echo "wazuh_command.remote_commands=1" >> /var/ossec/etc/local_internal_options.conf
 echo "<ossec_config>
     <client>
     <server>
-      <address>logs.vervantis.com</address>
+      <address></address>
       <port>1514</port>
       <protocol>tcp</protocol>
     </server>
