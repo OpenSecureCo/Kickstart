c1587b310c636ce7f4a9e40573b7a71aa1bfb8ea
OpenSecureCo <74164997+OpenSecureCo@users.noreply.github.com>
2021-11-17 17:55:55 -0600

    Update windowswazuhinstall.ps1

--- windowswazuhinstall.ps1
+++ windowswazuhinstall.ps1
@@ -6,7 +6,7 @@ Remove-Item 'C:\Program Files (x86)\ossec-agent' -Force -Recurse
 New-Item -Path "C:\" -Name "Wazuh" -ItemType "directory"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.2.4-1.msi -OutFile wazuh-agent.msi; ./wazuh-agent.msi /q WAZUH_MANAGER='w.g4ns.com' WAZUH_REGISTRATION_PASSWORD='ytrHWtpxQ986G8Jt' WAZUH_REGISTRATION_SERVER='w.g4ns.com' WAZUH_AGENT_GROUP='G4NS' 
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.2.5-1.msi -OutFile wazuh-agent.msi; ./wazuh-agent.msi /q WAZUH_MANAGER='192.168.200.16' WAZUH_REGISTRATION_SERVER='192.168.200.16' 
 
 
 <#
