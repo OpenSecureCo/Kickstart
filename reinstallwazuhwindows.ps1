1fe71673f5f8ba03ad181215a8e737eaee3b7b7a
OpenSecureCo <74164997+OpenSecureCo@users.noreply.github.com>
2021-12-03 14:30:16 -0600

    Create reinstallwazuhwindows.ps1

---
+++ reinstallwazuhwindows.ps1
@@ -0,0 +1,12 @@
+Stop-Service -Name "Wazuh"
+$MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Wazuh Agent"}
+$MyApp.Uninstall()
+Start-Sleep -s 15
+Remove-Item 'C:\Program Files (x86)\ossec-agent' -Force -Recurse
+New-Item -Path "C:\" -Name "Wazuh" -ItemType "directory"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.2.4-1.msi -OutFile wazuh-agent-4.2.4.msi; ./wazuh-agent-4.2.4.msi /q WAZUH_MANAGER='w.g4ns.com' WAZUH_REGISTRATION_SERVER='w.g4ns.com' WAZUH_REGISTRATION_PASSWORD='ytrHWtpxQ986G8Jt'
+
+
+
