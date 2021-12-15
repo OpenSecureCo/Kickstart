1bd6022507b44e1ded47f7ac4cbbe2ac0b51196a
OpenSecureCo <74164997+OpenSecureCo@users.noreply.github.com>
2021-12-15 16:07:58 -0600

    Update custom-suricata.sh

--- custom-suricata.sh
+++ custom-suricata.sh
@@ -9,6 +9,7 @@ PWD=`pwd`
 read -r INPUT_JSON
 SRCIP=$(echo $INPUT_JSON | jq -r .parameters.alert.data.src_ip)
 COMMAND=$(echo $INPUT_JSON | jq -r .command)
+ALERT=$(echo $INPUT_JSON | jq -r .parameters.alert.data.alert.signature)
 LOG_FILE="${PWD}/../logs/active-responses.log"
 
 #------------------------ Analyze command -------------------------#
@@ -17,12 +18,12 @@ if [ ${COMMAND} = "add" ]
 then
 /sbin/iptables -I INPUT -s ${SRCIP} -j DROP
 /sbin/iptables -I FORWARD -s ${SRCIP} -j DROP
-echo "`date` /var/ossec/$0 Source IP $SRCIP Added to Blacklist" >> ${LOG_FILE}
+echo "`date` /var/ossec/$0 Source IP $SRCIP Added to Blacklist due to alert $ALERT" >> ${LOG_FILE}
 #echo "$INPUT_JSON" > /tmp/alerts.log
 else
 /sbin/iptables -D INPUT -s ${SRCIP} -j DROP
 /sbin/iptables -D FORWARD -s ${SRCIP} -j DROP
-echo "`date` /var/ossec/$0 Source IP $SRCIP Removed from Blacklist" >> ${LOG_FILE}
+echo "`date` /var/ossec/$0 Source IP $SRCIP Removed from Blacklist due to alert $ALERT" >> ${LOG_FILE}
 #echo "$INPUT_JSON" > /tmp/alerts.log
 fi
 
